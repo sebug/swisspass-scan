a651fd6fa86fa778203f113be8235e7e00ca2090
sebug <sebastian.gfeller@gmail.com>
2019-04-21 20:29:34 +0200

    Allowing to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
     entry: './index.js',
     output: {
-	path: path.resolve(__dirname, 'dist');
+	path: path.resolve(__dirname, 'dist'),
 	filename: 'swisspass-scan.bundle.js'
-    }
+    },
+    mode: 'production'
 };
