d084ca3672a4b62a93ced6cdfe4b9081e978a598
sebug <sebastian.gfeller@gmail.com>
2019-04-21 20:34:27 +0200

    HTML output

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './index.js',
+    plugins: [
+	new HtmlWebpackPlugin({
+	    title: 'Swisspass Scan'
+	})
+    ],
     output: {
 	path: path.resolve(__dirname, 'dist'),
 	filename: 'swisspass-scan.bundle.js'
